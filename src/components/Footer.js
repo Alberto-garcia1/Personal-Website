7505db556e1755c24984abe8ca0608f2c4a5d366
Alberto Garcia <albertogarcia2220@gmail.com>
2024-01-24 15:09:08 -0700

    Created Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Instagram, LinkedIn, GitHub } from "@mui/icons-material";
+import "../styles/Footer.css";
+
+function Footer() {
+    const year = new Date().getFullYear();
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <a
+          href="https://www.instagram.com/alberto.g8/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Instagram />
+        </a>
+        <a
+          href="https://github.com/Alberto-garcia1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/alberto-garcia-98434720a"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedIn />
+        </a>
+      </div>
+      <p>Copyright ⓒ {year} Alberto Garcia</p>
+    </div>
+  );
+}
+
+export default Footer;
