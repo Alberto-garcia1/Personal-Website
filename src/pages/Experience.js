4a51635a69c52b74662d07ce885f89ab44825ce6
ALberto Garcia <albertogarcia2220@gmail.com>
2024-03-01 10:26:40 -0700

    Added work hitory portion

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -8,42 +8,45 @@ import SchoolIcon from '@mui/icons-material/School';
 import WorkHistoryOutlinedIcon from '@mui/icons-material/WorkHistoryOutlined';
 
 function Experience() {
-  // Array containing education data
+  // Array containing experience data
   const experienceData = [
     {
+      type: "education",
       date: "January 2023 - May 2023",
       institution: "Pueblo Community College, Colorado",
-      certification: "CompTIA A+ Certification",
+      outcome: "CompTIA A+ Certification",
     },
     {
+      type: "education",
       date: "January 2023 - May 2023",
       institution: "Nucamp Coding Bootcamp",
-      certification: "Front-End Web Development with Google Cloud",
+      outcome: "Front-End Web Development with Google Cloud",
     },
     {
-      date: "Febuary 2024 - Present",
-      occupation: "Freelance Web Developer",
-      certification: "Front-End Web Development with Google Cloud",
+      type: "work",
+      date: "February 2024 - Present",
+      institution: "Freelance Web Developer",
+      outcome: "Rosie Bake Me A Cake Website",
     },
   ];
 
   return (
     <div className="experience">
-      {/* Vertical timeline to display education history */}
+      {/* Vertical timeline to display experience history */}
       <VerticalTimeline lineColor="#272727">
         {experienceData.map((item, index) => (
           <VerticalTimelineElement
             key={index}
             // Add even/odd class for alternating timeline elements
-            className={`vertical-timeline-element--education ${
+            className={`vertical-timeline-element--${item.type} ${
               index % 2 === 0 ? 'even' : 'odd'
             }`}
             date={item.date} // Display date
             iconStyle={{ background: '#272727', color: '#fff' }} // Style for the timeline icon
-            icon={<SchoolIcon />} // Icon for education events
+            icon={item.type === 'education' ? <SchoolIcon /> : <WorkHistoryOutlinedIcon />} // Icon for education or work experience
           >
-            <h3 className="vertical-timeline-element-title">{item.institution}</h3>
-            <p>{item.certification}</p> {/* Display certification*/}
+            <h3 className="vertical-timeline-element-title">{item.institution || item.outcome}</h3>
+            <p>{item.outcome}</p> {/* Display certification*/}
           </VerticalTimelineElement>
         ))}
       </VerticalTimeline>
